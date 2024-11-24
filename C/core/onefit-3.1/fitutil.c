02b254f5978bc8e1840c05ba6d0033a43c5fbcda
Pedro Sebastiao <pjsebastiao@gmail.com>
2024-11-24 18:31:20 +0000

    0.8.14 compile warning about ignered return values in scanf calls corrected

--- C/core/onefit-3.1/fitutil.c
+++ C/core/onefit-3.1/fitutil.c
@@ -703,9 +703,9 @@ FILE   *f;
 {
    double   x;
    char   c;
-   int err = 1;
+
    while( (c=fgetc(f)) != '=');
-   if (!fscanf(f,"%lf",&x)) printf("error in fitutil.c, lernum()\n");
+   if (!fscanf(f,"%lf",&x)) printf("fscanf() call error in fitutil.c, lernum()\n");
    return x;
 }
 /****************************************************************************/
