c8196dd359289b68888ff03bb31938a8cbb6eb30
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-18 21:51:34 +0100

    0.9.72

--- C/local/BPP.c
+++ C/local/BPP.c
@@ -3,13 +3,13 @@
 
 #define pi 3.1415927  
 
+extern lor(double *t, double w);
 
 double BPP(double f,double a,double tauc)
 {
+	double w,af;
+	w = 2*pi*f;
+	af  = a*(lor(tauc,w)+4*lor(tauc,2*w));
 
-double w,af,lor();
-w = 2*pi*f;
-af  = a*(lor(tauc,w)+4*lor(tauc,2*w));
-
-return af;
+	return af;
 }
