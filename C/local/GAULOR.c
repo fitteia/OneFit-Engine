8ca1a37db39a2f845e6443240d6dfa74e3de27de
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-19 18:44:20 +0100

    0.9.72

--- C/local/GAULOR.c
+++ C/local/GAULOR.c
@@ -1,13 +1,13 @@
 #include <math.h>
 #include <stdio.h>
 
+extern double GAUSS(double f, double a, double b, double f0, double Df);
+extern double LORTZN(double f, double a, double b, double f0);
+
 double GAULOR(double f, double a, double b, double f0, double t)
 {
-  double	GAUSS1(),LORTZN();
-
   if(t>0.5){ return LORTZN(f,a,b,f0); }
   else { return  GAUSS1(f,a,b,f0); }
-
 }
 
 
