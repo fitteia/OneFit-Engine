6c65292e2fed810a1524ba06079850d4738fdb49
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-23 19:54:18 +0100

    0.9.73

--- C/core/onefit-3.1/modelos/t1vold_new.h
+++ C/core/onefit-3.1/modelos/t1vold_new.h
@@ -3,7 +3,7 @@
 #define DIM 10
 
 int t1vold_new_(double *f, double *delta, double *S, double *Tau_S, double *Tau_L, double *k_1, double *p_p, 
-				double a[DIM][DIM][DIM], double b[DIM][DIM][DIM], double c[DIM][DIM], double *t1_ang, double j0, double j1, double j2, 
+				double a[DIM][DIM][DIM], double b[DIM][DIM][DIM], double c[DIM][DIM], double *t1_ang, double *j0, double *j1, double *j2, 
 				double *a_fact0, double *a_fact1, double *a_fact2);
 
 
