ae5e072bc9c457398d2e48b64b84cf7dfb2dd3c2
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-23 15:52:57 +0100

    0.9.73

---
+++ C/local/RMTDnum.h
@@ -0,0 +1,8 @@
+/** RMTDnum.h **/
+
+
+double RMTDnum(double f, double p, double Armtd, double fcm,double fcM);
+double jrmtd_int1(Function *X);
+double jrmtd(double f,double fcM,double fcm,double p);
+double jrmtd_int(Function *X);
+
