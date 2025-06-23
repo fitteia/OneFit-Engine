21532bf4d8404d029f84c96487eb3ac5523f9a42
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-23 20:54:08 +0100

    0.9.73

---
+++ C/local/SDFreed.h
@@ -0,0 +1,7 @@
+/** SDFreed.h **/
+
+double SDFreed(double f, double tau, double n, double d, double NP);
+double SDFreedHF(double f1, double f2, double tau, double n, double d, double NP);
+double __JSDFreed_(double f, double tau, double NP);
+double __QSDFreed_(double wt, double NP) ;
+double __integral0_(Function *X);
