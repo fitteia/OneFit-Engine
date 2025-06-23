4584cbb16d547738974b971fcf11635926fab5e0
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-23 20:30:25 +0100

    0.9.73

--- C/core/onefit-3.1/modelos/t1vold_abc.h
+++ C/core/onefit-3.1/modelos/t1vold_abc.h
@@ -6,8 +6,8 @@
 
 int t1vold_abc_( double *f, double *S, double a[D3][D3][D3], double b[D3][D3][D3], double c[D3][D3],  double (*a00_1)[D2], double (*a00_2)[D2], double (*a10_1)[D2], 
 				   double (*a10_2)[D2], double (*a11_1)[D2], double (*a11_2)[D2], double (*a11_3)[D2], double (*a12_1)[D2], double (*a12_2)[D2], double (*a12_3)[D2],
-				   double  a20_1[D1][D2], double a20_2[D1][D2], double a22_1[D1][D2], double b00_1[D1][D2], double b00_2[D1][D2], double b10_1[D1][D2], double b10_2[D1][D2],
-				   double  b11_1[D1][D2], double b11_2[D1][D2], double b11_3[D1][D2], double b12_1[D1][D2], double b12_2[D1][D2], double b12_3[D1][D2], double b20_1[D1][D2],
-				   double b20_2[D1][D2], double b22_1[D1][D2],  double c00[D1][D2], double c10[D1][D2], double c11[D1][D2], double c12[D1][D2], double c20[D1][D2], double c22[D1][D2]);
+				   double (*a20_1)[D2], double (*a20_2)[D2], double (*a22_1)[D2], double (*b00_1)[D2], double (*b00_2)[D2], double (*b10_1)[D2], double (*b10_2)[D2],
+				   double (*b11_1)[D2], double (*b11_2)[D2], double (*b11_3)[D2], double (*b12_1)[D2], double (*b12_2)[D2], double (*b12_3)[D2], double (*b20_1)[D2],
+				   double (*b20_2)[D2], double (*b22_1)[D2], double (*c00)[D2], double (*c10)[D2], double (*c11)[D2], double (*c12)[D2], double (*c20)[D2], double (*c22)[D2]);
 
 
