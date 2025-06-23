41597804700ce1fbda7c44c1750fa633d842d054
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-23 20:37:13 +0100

    0.9.73

--- C/local/RotVold.c
+++ C/local/RotVold.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include "RotVold.h"
 #include <stdio.h>
 
 #define pi 3.1415927  
@@ -6,7 +7,5 @@
 
 double RotVold(double f,double delta, double Szz, double tauS,  double tauL, double k1, double p, double A0, double A1, double A2)
 {
-  double T1_rot_vold();
-
   return  T1_rot_vold(f,delta,Szz,tauS,tauL,k1,p,A0,A1,A2);
 } 
