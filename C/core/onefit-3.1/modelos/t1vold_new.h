b4a23780fc9721c25409f99c92b6700cd508ac05
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-23 18:25:31 +0100

    0.9.73

--- C/core/onefit-3.1/modelos/t1vold_new.h
+++ C/core/onefit-3.1/modelos/t1vold_new.h
@@ -1,6 +1,8 @@
 /** t1vold_new.h **/
 
 
-int t1vold_new_( double f, double delta, double S, double Tau_S, double Tau_L, double k_1, double p_p, double a, double b, double c, double t1_ang, double j0, double j1, double j2, double a_fact0, double a_fact1, double a_fact2);
+int t1vold_new_(double *f, double *delta, double *S, double *Tau_S, double *Tau_L, double *k_1, double *p_p, 
+				double a, double b, double c, double t1_ang, double j0, double j1, double j2, 
+				double *a_fact0, double *a_fact1, double *a_fact2);
 
 
