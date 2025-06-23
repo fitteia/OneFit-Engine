bacbf48c59153e42168349546c47a06a516c2c28
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-23 18:37:19 +0100

    0.9.73

--- C/core/onefit-3.1/modelos/t1vold_abc.h
+++ C/core/onefit-3.1/modelos/t1vold_abc.h
@@ -1,11 +1,12 @@
 /** t1vold_new.h **/
 
-int t1vold_abc_( double f, double S, double a, double b, double c,  double a00_1, double a00_2, double a10_1, 
-				   double a10_2, double a11_1, double a11_2, double a11_3, double a12_1, double a12_2, double a12_3,
-				   double  a20_1, double a20_2, double a22_1, double b00_1, double b00_2, double b10_1, double b10_2,
-				   double  b11_1, double b11_2, double b11_3, double b12_1, double b12_2, double b12_3, double b20_1,
-				   double b20_2, double b22_1,  double c00, double c10, double c11, double c12, double c20, double c22);
+#define D1 2
+#define D2 30
 
-
+int t1vold_abc_( double *f, double *S, double a, double b, double c,  double a00_1[D1][D2], double a00_2[D1][D2], double a10_1[D1][D2], 
+				   double a10_2[D1][D2], double a11_1[D1][D2], double a11_2[D1][D2], double a11_3[D1][D2], double a12_1[D1][D2], double a12_2[D1][D2], double a12_3[D1][D2],
+				   double  a20_1[D1][D2], double a20_2[D1][D2], double a22_1[D1][D2], double b00_1[D1][D2], double b00_2[D1][D2], double b10_1[D1][D2], double b10_2[D1][D2],
+				   double  b11_1[D1][D2], double b11_2[D1][D2], double b11_3[D1][D2], double b12_1[D1][D2], double b12_2[D1][D2], double b12_3[D1][D2], double b20_1[D1][D2],
+				   double b20_2[D1][D2], double b22_1[D1][D2],  double c00[D1][D2], double c10[D1][D2], double c11[D1][D2], double c12[D1][D2], double c20[D1][D2], double c22[D1][D2]);
 
 
