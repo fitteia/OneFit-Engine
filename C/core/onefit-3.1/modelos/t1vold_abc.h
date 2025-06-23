f1f38f7fbc03636efc4dcf61ba97f8ae9d576a12
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-23 18:47:50 +0100

    0.9.73

--- C/core/onefit-3.1/modelos/t1vold_abc.h
+++ C/core/onefit-3.1/modelos/t1vold_abc.h
@@ -4,7 +4,7 @@
 #define D2 2
 #define D3 10
 
-int t1vold_abc_( double *f, double *S, double a[D3][D3][D3], double b[D3][D3][D3], double c[D3][D3][D3],  double a00_1[D1][D2], double a00_2[D1][D2], double a10_1[D1][D2], 
+int t1vold_abc_( double *f, double *S, double a[D3][D3][D3], double b[D3][D3][D3], double c[D3][D3],  double a00_1[D1][D2], double a00_2[D1][D2], double a10_1[D1][D2], 
 				   double a10_2[D1][D2], double a11_1[D1][D2], double a11_2[D1][D2], double a11_3[D1][D2], double a12_1[D1][D2], double a12_2[D1][D2], double a12_3[D1][D2],
 				   double  a20_1[D1][D2], double a20_2[D1][D2], double a22_1[D1][D2], double b00_1[D1][D2], double b00_2[D1][D2], double b10_1[D1][D2], double b10_2[D1][D2],
 				   double  b11_1[D1][D2], double b11_2[D1][D2], double b11_3[D1][D2], double b12_1[D1][D2], double b12_2[D1][D2], double b12_3[D1][D2], double b20_1[D1][D2],
