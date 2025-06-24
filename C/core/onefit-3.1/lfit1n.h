cede4568237fc9ca81bba629bafcdd62c4494099
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-24 17:28:28 +0100

    0.9.73

--- C/core/onefit-3.1/lfit1n.h
+++ C/core/onefit-3.1/lfit1n.h
@@ -2,6 +2,6 @@
 
 void lfit1n( double x[], double y[], double sig[], int ndata, double a[], int ma, int lista[], 
 			 int mfit, double *chisq,
-			 void (*func_ptr)(double x,double *afunc, int ma, double t, int mode), 
+			 void (*func_ptr)(double x,double *afunc, int ma, double *p, double t, int mode), 
 			 double par[], double t, int mode);
 
