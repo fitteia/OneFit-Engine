0ab0ba0ccd926c9cc5c86d02a17d0a4706c09563
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-24 10:47:51 +0100

    0.9.73

--- C/core/onefit-3.1/fitutil.h
+++ C/core/onefit-3.1/fitutil.h
@@ -4,4 +4,5 @@ void nrerror(char error_text[]);
 double *dvector(int nl, int nh);
 void dpolint(double xa[],double ya[],int n, double x, double *y, double *dy);
 void free_dvector(double *v, int nl, int nh);
-void dsplint(double xa[], double ya[], double y2a[], int n, double x, double *y)
+void dsplint(double xa[], double ya[], double y2a[], int n, double x, double *y);
+
