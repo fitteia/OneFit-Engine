d16a06cbaebcd07ac24b4ed4c12dcce934e2fc38
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-24 14:15:16 +0100

    0.9.73

--- C/core/onefit-3.1/actpar.c
+++ C/core/onefit-3.1/actpar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "fitutil.h"
 #include "actpar.h"
@@ -10,7 +11,7 @@ void usage()
   exit(0);
 }
 
-void main( int argc,char *argv[])
+int main( int argc,char *argv[])
 // int argc;
 // char *argv[];
 {
