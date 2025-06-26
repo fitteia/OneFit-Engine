cd4d8fde7f60574624a6c89c646ccd568bd53b3a
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-26 17:33:00 +0100

    0.9.73

---
+++ C/core/onefit-3.1/modelos/sdvilf5.h
@@ -0,0 +1,5 @@
+// sdvilf5.h
+double jw5(int k,double t,double w);
+double jwd5(int i,double t,double w,double delta);
+double sdvilf5(double I,double d,double n,double t,double w,double delta);
+double t1ro5(double I,double d,double n,double t,double w,double w0,double delta);
