b5c408f891bf8790f2d013b614c09ed318b9ac3c
Pedro Sebastiao <pjsebastiao@gmail.com>
2025-06-26 18:03:52 +0100

    0.9.73

--- C/core/onefit-3.1/modelos/sdvilfsa13_0.c
+++ C/core/onefit-3.1/modelos/sdvilfsa13_0.c
@@ -5,6 +5,8 @@
 /******************************************************************************/
 #include <math.h>
 #include <stdio.h>
+#include "sdvilfsa13_0.h"
+#include "fitutil.h"
 
 #define	C	0.569504e-12
 #define ndt1	28
@@ -44,12 +46,11 @@ static double	jwdev[][7]={
 /******************************************************************************/
 /*									      */
 /******************************************************************************/
-double sdvilfSa13_0(I,d,n,t,w,delta)
-double I,d,n,t,w,delta;
+double sdvilfSa13_0(double I,double d,double n,double t,double w,double delta)
+// double I,d,n,t,w,delta;
 {
 	int	j;
 	double	B,D,wt,m,j1,j2,af;
-	double	fij(),jw13_0();
 
 	if(delta == 0.0){
 		af = jw13_0(1,t,w) + jw13_0(2,t,2*w);
@@ -69,14 +70,12 @@ double I,d,n,t,w,delta;
 /******************************************************************************/
 /*									      */
 /******************************************************************************/
-double jw13_0(k,t,w)
-int	k;
-double t,w;
+double jw13_0(int k,double t,double w)
+// int	k;
+// double t,w;
 {
 	int	j,i,p;
 	double	wt,m,af,*x,*y,*y2,*xx,*yy,ypn;
-	double	*dvector(),dpolint();
-	void	free_dvector(),dsplint();
 
 	x  = dvector(1,Ndt+1);
 	y  = dvector(1,Ndt+1);
@@ -115,12 +114,12 @@ double t,w;
 /******************************************************************************/
 /*									      */
 /******************************************************************************/
-double	jwd11(i,t,w,delta)
-int	i;
-double	t,w,delta;
+double	jwd11(int i,double t,double w,double delta)
+// int	i;
+// double	t,w,delta;
 {
 	int	j;
-	double	af,jw13_0(),fij();
+	double	af;
 
 	for(j=0,af=0.0;j<=2;j++) af += fij(i,j,delta)*jw13_0(j,t,w);
 	return af;
@@ -128,13 +127,12 @@ double	t,w,delta;
 /******************************************************************************/
 /*									      */
 /******************************************************************************/
-double t1ro11(I,d,n,t,w,w0,delta)
-double I,d,n,t,w,w0,delta;
+double t1ro11(double I,double d,double n,double t,double w,double w0,double delta)
+// double I,d,n,t,w,w0,delta;
 /* B*(J0(2*w1)+10*J1(w0)+J2(2*w0) */
 {
 	int	j;
 	double	B,D,wt,w0t,w2t,m,J0,J1,J2,af;
-	double	jwd11();
 
 	J0 = jwd11(0,t,2*w,delta);
 	J1 = jwd11(1,t,w0,delta);
