c1513d120c86808ffe9102ff1186259b7f486e0e
Pedro Sebastiao <pj.sebastiao@gmail.com>
2025-12-05 17:51:42 +0300

    0.9.117

--- C/local/Florence_c.c
+++ C/local/Florence_c.c
@@ -104,5 +104,6 @@ double florence(
       aux=aux/CONCM*0.001 + R1[4];
     printf("%lg %lg %lg %lg %lg %lg aux=%lg\n",pow(10,FREQ),R1[0],R1[1],R1[2],R1[3],R1[4],aux);
 	// printf("%lg %lg\n",FREQ,R1);
+	R1[0]=-1.0;
  	return R1[0];	
 }
