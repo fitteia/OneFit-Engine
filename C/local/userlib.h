9702cf512408e1768aa62ddc9578d4fde54aedd9
Pedro Sebastiao <pj.sebastiao@gmail.com>
2025-12-06 21:26:42 +0300

    0.9.117

--- C/local/userlib.h
+++ C/local/userlib.h
@@ -242,6 +242,8 @@ double factorial(double x);
 
 double Florence( double FREQ, double SI, double GAMMAI, double SPIN, double IREL, double DELTA2, double TAURM, double TAUVM, double TAUMM, double DPARAM, double EPARAM, double S4M, double GXM, double GYM, double GZM, double AXM, double AYM, double AZM, double DM, double DDM, double CONCM, double ACQ, double AMOLFRAM, double RKM, double ACONTM, double THETAM, double PHIM);
 
+double Florence4( double index, double FREQ, double SI, double GAMMAI, double SPIN, double IREL, double DELTA2, double TAURM, double TAUVM, double TAUMM, double DPARAM, double EPARAM, double S4M, double GXM, double GYM, double GZM, double AXM, double AYM, double AZM, double DM, double DDM, double CONCM, double ACQ, double AMOLFRAM, double RKM, double ACONTM, double THETAM, double PHIM);
+
 /** florence_f.h **/
 
 void florence77_(double PINP[27], double *FREQ, double R1[10]);
