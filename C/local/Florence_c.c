5b3220a8f5e9dda18b6630d8271ffadff7f2a673
Pedro Sebastiao <pj.sebastiao@gmail.com>
2025-12-07 13:02:11 +0300

    0.9.117

--- C/local/Florence_c.c
+++ C/local/Florence_c.c
@@ -34,6 +34,102 @@
       PHIM(J)    = 0
 **/
 
+double FlorenceN(
+	  double index,
+	  double n,
+      double FREQ,
+      double SI,
+      double GAMMAI,
+      double SPIN,
+      double IREL,
+      double DELTA2,
+      double TAURM,
+      double TAUVM,
+      double TAUMM,
+      double DPARAM,
+      double EPARAM,
+      double S4M,
+      double GXM,
+      double GYM,
+      double GZM,
+      double AXM,
+      double AYM,
+      double AZM,
+      double DM,
+      double DDM,
+      double CONCM,
+      double ACQ,
+      double AMOLFRAM,
+      double RKM,
+      double ACONTM,
+      double THETAM,
+      double PHIM
+)
+{
+	static double R1[]={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+   	static int count = 0;	
+	double PINP[27];
+    double aux;
+
+	if (count < 1 || count > (int) n-1 ) {
+		FREQ = log10(FREQ);
+
+     	PINP[0] = SI;
+    	PINP[1] = GAMMAI;
+   		PINP[2] = SPIN;
+      	PINP[3] = IREL;
+      	PINP[4] = DELTA2; // TAUS0M
+      	PINP[5] = TAURM;
+      	PINP[6] = TAUVM;
+      	PINP[7] = TAUMM;
+      	PINP[8] = 2.0; // TAUDELTA // TO FORCE PINP[4] TO BE DELTA2
+      	PINP[9] = DPARAM;
+      	PINP[10] = EPARAM;
+      	PINP[11] = S4M;
+      	PINP[12] = GXM;
+      	PINP[13] = GYM;
+      	PINP[14] = GZM;
+      	PINP[15] = AXM;
+      	PINP[16] = AYM;
+      	PINP[17] = AZM;
+      	PINP[18] = DM;
+      	PINP[19] = DDM;
+      	PINP[20] = CONCM;
+      	PINP[21] = ACQ;
+      	PINP[22] = AMOLFRAM;
+      	PINP[23] = RKM;
+      	PINP[24] = ACONTM;
+      	PINP[25] = THETAM;
+      	PINP[26] = PHIM;
+
+		florencef77_(PINP,&FREQ,R1);
+  		aux=R1[1]+R1[2]+R1[3];
+ 		count = 0;
+	}
+	switch ( (int) index ) {
+		default:
+			aux = R1[0];
+			break;
+		case 1:
+			count++;
+			aux = R1[1];
+			break;
+		case 2:
+			count++;
+			aux = R1[2];
+			break;
+		case 3:
+			count++;
+			aux = R1[3];
+			break;
+		case 4:
+			count++;
+			aux = R1[4];
+			break;
+	}
+	return aux;
+}
+
 double Florence(
       double FREQ,
       double SI,
