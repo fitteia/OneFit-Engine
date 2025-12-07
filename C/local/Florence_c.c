4e67ebd94ea86030990bc4c0c7d7ef08fb1cc41c
Pedro Sebastiao <pj.sebastiao@gmail.com>
2025-12-07 17:48:37 +0300

    0.9.117

--- C/local/Florence_c.c
+++ C/local/Florence_c.c
@@ -103,7 +103,12 @@ double FlorenceN(
       	PINP[25] = THETAM;
       	PINP[26] = PHIM;
 
+		printf("before: %lg %lg %lg %lg %lg %lg %lg RkM=%lg\n", FREQ,R1[0],R1[1],R1[2],R1[3],R1[4],R1[5],PINP[23]);
 		florencef77_(PINP,&FREQ,R1);
+		// for (i=0; i<10; i++) { R1[i] = aR1[i]; }	
+ 
+		florencef77_(PINP,&FREQ,R1);
+		printf("after: %lg %lg %lg %lg %lg %lg %lg RKM=%lg\nreset count: %d\n", FREQ,R1[0],R1[1],R1[2],R1[3],R1[4],R1[5],PINP[23], count);
 	//	printf("reset count: %d\n%lg %lg %lg %lg %lg %lg %lg\n",count, FREQ,R1[0],R1[1],R1[2],R1[3],R1[4],R1[5]);
  		count = 0;
 	}
